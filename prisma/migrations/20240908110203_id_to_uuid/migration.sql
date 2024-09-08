6c7855180edaaa45aa423ece61b09a92761e61b4
kimkiyong <fenderst0612@gmail.com>
2024-09-08 20:02:23 +0900

    refactor: Change primary key types and add foreign key constraints
    This commit modifies the primary key types for the `Post`, `Profile`, `Tag`, and `User` tables in the database. The primary key columns are changed from `Int` to `UUID`. Additionally, foreign key constraints are added to ensure referential integrity.

---
+++ prisma/migrations/20240908110203_id_to_uuid/migration.sql
@@ -0,0 +1,83 @@
+/*
+  Warnings:
+
+  - The primary key for the `Post` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Profile` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `Tag` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - Changed the type of `id` on the `Post` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `authorId` on the `Post` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `id` on the `Profile` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `userId` on the `Profile` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `id` on the `Tag` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `id` on the `User` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `A` on the `_PostToTag` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `B` on the `_PostToTag` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_userId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_PostToTag" DROP CONSTRAINT "_PostToTag_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_PostToTag" DROP CONSTRAINT "_PostToTag_B_fkey";
+
+-- AlterTable
+ALTER TABLE "Post" DROP CONSTRAINT "Post_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+DROP COLUMN "authorId",
+ADD COLUMN     "authorId" UUID NOT NULL,
+ADD CONSTRAINT "Post_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "Profile" DROP CONSTRAINT "Profile_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+DROP COLUMN "userId",
+ADD COLUMN     "userId" UUID NOT NULL,
+ADD CONSTRAINT "Profile_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "Tag" DROP CONSTRAINT "Tag_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+ADD CONSTRAINT "Tag_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "_PostToTag" DROP COLUMN "A",
+ADD COLUMN     "A" UUID NOT NULL,
+DROP COLUMN "B",
+ADD COLUMN     "B" UUID NOT NULL;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_PostToTag_AB_unique" ON "_PostToTag"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_PostToTag_B_index" ON "_PostToTag"("B");
+
+-- AddForeignKey
+ALTER TABLE "Post" ADD CONSTRAINT "Post_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostToTag" ADD CONSTRAINT "_PostToTag_A_fkey" FOREIGN KEY ("A") REFERENCES "Post"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_PostToTag" ADD CONSTRAINT "_PostToTag_B_fkey" FOREIGN KEY ("B") REFERENCES "Tag"("id") ON DELETE CASCADE ON UPDATE CASCADE;
